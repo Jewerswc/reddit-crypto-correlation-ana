fd0a4babd995072662eccde8996433be8dc0a87b
William <william.jewers@gmail.com>
2025-01-14 17:52:14 +0000

    Add analysis/correlation_analysis.py

---
+++ analysis/correlation_analysis.py
@@ -0,0 +1,6 @@
+# Python script to analyze the correlation between Reddit engagement and market trends
+import numpy as np
+from scipy.stats import pearsonr
+
+def calculate_correlation(data):
+    # Your code here
