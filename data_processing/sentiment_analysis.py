ac9dedbb8bcb40e70d2ab18396540bcba0663a62
William <william.jewers@gmail.com>
2025-01-14 17:52:11 +0000

    Add data_processing/sentiment_analysis.py

---
+++ data_processing/sentiment_analysis.py
@@ -0,0 +1,5 @@
+# Python script to perform sentiment analysis on Reddit post titles and comments
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+def analyze_sentiment(text):
+    # Your code here
