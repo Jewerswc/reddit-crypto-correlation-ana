a4f96c5cdbe6d708a67a5f7c76b35ae9e445fd6b
William <william.jewers@gmail.com>
2025-01-14 17:52:15 +0000

    Add analysis/predictive_modeling.py

---
+++ analysis/predictive_modeling.py
@@ -0,0 +1,6 @@
+# Python script for predictive modeling using machine learning
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def build_model(data):
+    # Your code here
